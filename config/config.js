f67a7c7be42d3794ae45cc3de1fdac51097c165b
stazman <cadistasio@gmail.com>
2024-02-09 23:10:44 -0600

    creates config.js. migrates, seeds; adds react client

---
+++ config/config.js
@@ -0,0 +1,31 @@
+require('dotenv').config(); // This line is important!
+
+module.exports = {
+  development: {
+    username: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE,
+    host: process.env.PG_HOST,
+    dialect: 'postgres', // or 'postgres', 'sqlite', etc.
+  },
+  test: {
+    username: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE,
+    host: process.env.PG_HOST,
+    dialect: 'postgres', // or whatever dialect you are using
+  },
+  production: {
+    username: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+};
