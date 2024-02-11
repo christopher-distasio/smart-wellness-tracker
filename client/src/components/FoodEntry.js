e1a62c9c9f3aee01b764b99366cfe251e04b0927
Chris DiStasio <stahzz1970@gmail.com>
2024-02-11 17:11:50 -0600

    Adds cors; food, home jsx; fetches, shows  entries

---
+++ client/src/components/FoodEntry.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+
+function FoodEntry() {
+
+  let [foodEntries, setFoodEntries] = useState();
+
+  useEffect(() => {
+    const foodApi = `http://localhost:4000/foodEntries`;
+
+    fetch(foodApi)
+      .then((res) => res.json())
+      .then((data) => {
+        setFoodEntries(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+      
+  }, []);
+
+  return (
+    <div>
+      <h1>Food Entry Page</h1>
+
+      <h2>All Your Food Entries:</h2>
+
+      <ul>
+        {foodEntries &&
+          foodEntries.map((foodEntry) => (
+            <>
+              <li key={foodEntry.id}>{foodEntry.food_name}</li>
+              <li key={foodEntry.id}>{foodEntry.calories}</li>
+              <li key={foodEntry.id}>{foodEntry.entry_date}<br></br><br></br></li>
+            </>
+          ))}
+      </ul>
+    </div>
+  );
+}
+
+export default FoodEntry;
--- server.js
+++ server.js
@@ -1,17 +1,41 @@
 const express = require("express");
 const app = express();
 const { Sequelize } = require("sequelize");
+// Add CORS
+const cors = require("cors");
 
 //config
+
+
 require("dotenv").config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 
+
+// app.use(express.static('public'));
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+// });
+
 //root
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Add this line to allow all origins
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); // Add this line to allow specific headers
+  next();
+});
+
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
+
+
 app.get("/", (req, res) => {
-  res.send("Hello World");
-})
+  res.send("Cannot GET /");
+});
+
+
 
 //controllers
 
