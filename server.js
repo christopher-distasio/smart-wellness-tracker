85db163681de96ca38ea4e76ea311ec1344ab53c
Chris DiStasio <stahzz1970@gmail.com>
2024-02-12 11:52:44 -0600

    Adds static file/script functionality for prod

--- server.js
+++ server.js
@@ -6,12 +6,9 @@ const cors = require("cors");
 
 //config
 
-
 require("dotenv").config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-
 
 // app.use(express.static('public'));
 
@@ -22,26 +19,34 @@ app.use(express.urlencoded({ extended: false }));
 //root
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*"); // Add this line to allow all origins
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); // Add this line to allow specific headers
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  ); // Add this line to allow specific headers
   next();
 });
 
-app.use(cors({
-  origin: 'http://localhost:3000'
-}));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
+app.use(express.static(path.join(__dirname, "client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
 
 app.get("/", (req, res) => {
   res.send("Cannot GET /");
 });
 
-
-
 //controllers
 
-const foodEntriesController = require("./controllers/foodEntriesController.js")
+const foodEntriesController = require("./controllers/foodEntriesController.js");
 app.use("/foodEntries", foodEntriesController);
 
 app.listen(process.env.PORT, () =>
-  console.log((`Running on port: ${process.env.PORT}`))
+  console.log(`Running on port: ${process.env.PORT}`)
 );
